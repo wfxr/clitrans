243a605fb05e2ba5e60ff8a50e802f3b74ec3703
Wenxuan Zhang <wenxuangm@gmail.com>
2021-03-08 17:27:17 +0800

    feat: parse opts using clap

---
+++ src/cli.rs
@@ -0,0 +1,17 @@
+use clap::AppSettings;
+
+pub use clap::Clap;
+
+#[derive(Clap, Debug)]
+#[clap( name = env!("CARGO_PKG_NAME"),
+       about = env!("CARGO_PKG_DESCRIPTION"),
+      author = env!("CARGO_PKG_AUTHORS"),
+     version = env!("CARGO_PKG_VERSION"),
+     global_setting(AppSettings::ColoredHelp),
+)]
+
+pub struct Opts {
+    /// Text to translate
+    #[clap(name = "QUERY")]
+    pub query: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+mod cli;
+
+use cli::{Clap, Opts};
 use clitrans::{bing::Translator, Parser};
-use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let query = env::args().nth(1).expect("missing query");
+    let opts: Opts = Opts::parse();
+    let query = opts.query;
     let url = format!("https://cn.bing.com/dict/search?q={}&mkt={}", query, "zh-cn");
     let resp = reqwest::get(&url).await?.text().await?;
     let trans = Translator::parse(&resp);
